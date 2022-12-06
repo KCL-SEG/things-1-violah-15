ca6325439033a195e399aaf6980b15aef43da8c8
“VIOLA <viola.harperath@kcl.ac.uk>
2022-12-06 18:43:51 +0000

    Fix constraint for quantity

--- things/models.py
+++ things/models.py
@@ -4,4 +4,4 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Thing(models.Model):
     name = models.CharField(unique = True, max_length=30, blank = False)
     description = models.CharField(max_length=120, blank = True)
-    quantity = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
