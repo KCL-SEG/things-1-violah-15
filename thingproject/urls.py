9f3effad60acda9626372568cdb8aa1c77a690d1
“VIOLA <viola.harperath@kcl.ac.uk>
2022-12-06 18:18:35 +0000

    Add import to urls.py

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from things import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view.home, name='home')
+    path('', views.home, name='home'),
 ]
